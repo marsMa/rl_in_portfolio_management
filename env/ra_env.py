7902958b70f6c097f51919756ddd11becb6f8ffb
mars.ma <mars.ma@thizgroup.com>
2018-12-07 21:05:25 +0800

    data

--- env/ra_env.py
+++ env/ra_env.py
@@ -1,9 +1,12 @@
 import gym
+import pandas as pd
 
 class ra_env(gym.Env):
     def __init__(self):
+        self.stock_price=pd.read_csv('data/all_stocks_5yr.csv')
 
     def step(self,action):
+
         return self.state,reward,done,{}
 
     def reset(self):
