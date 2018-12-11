4fc99495fcc44d3dc7c78d8c19589664fb93f1af
unknown <ustcmj@gmail.com>
2018-12-11 22:52:33 +0800

    merge1

--- env/ra_env.py
+++ env/ra_env.py
@@ -3,9 +3,51 @@ import pandas as pd
 
 class ra_env(gym.Env):
     def __init__(self):
-        self.stock_price=pd.read_csv('data/all_stocks_5yr.csv')
+        #env
+        self.fee_buy=0.005
 
+        #state
+        self.stock_price=pd.read_csv('data/all_stocks_5yr.csv')
+        self.stock=self.stock_price['Name'].unique()
+        self.n=len(self.stock_price['Name'].unique())
+        
+        self.stock_price['trade_price']=self.stock_price['open']
+        self.stock_price_col=self.stock_price.pivot_table(index='date',columns='Name',values='close')
+        self.index_price=pd.read_csv('')
+        #timeindex 2 int
+        self.time_step=self.stock_price['date'].sort().unique()
+
+        self.pfo=[0]*self.n
+        self.asset=1000000
+        #action
+        self.action=[0]*self.n
+        
+        #reward
+        self.reward=0
+
+
+    def find_state(time_step):
+        
+
+    
+    #action to reward, state
     def step(self,action):
+        #action->switch portfolio
+        trade_list=action-self.pfo
+        trade_fee=trade_list[trade_list>0].sum()*self.asset*self.fee_buy
+
+
+
+
+        #change state
+        self.time_step+=timedelta(day=1)
+
+        reward=action*(self.state['close']/self.state['open']).sum()*self.asset-trade_fee
+        done=True
+
+        self.time_step+=1
+        self.state=find_state(time_step)
+
 
         return self.state,reward,done,{}
 
