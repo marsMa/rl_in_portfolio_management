d8b1e7584402d4145ed6e3aee549f1eb69a9890c
mars.ma <mars.ma@thizgroup.com>
2018-12-11 22:11:59 +0800

    env

--- env/ra_env.py
+++ env/ra_env.py
@@ -1,10 +1,14 @@
 import gym
 import pandas as pd
+import numpy as np
 
 class ra_env(gym.Env):
     def __init__(self):
         self.stock_price=pd.read_csv('data/all_stocks_5yr.csv')
-
+        self.len=len(self.stock_price['name'].unique())
+        self.pfo=np.array([0])*self.len
+        self.state=self.price+self.pfo
+        
     def step(self,action):
 
         return self.state,reward,done,{}
